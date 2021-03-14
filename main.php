46ae8a144634ca515b854fbd0d0323295ff43560
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2021-03-14 19:27:51 +0100

    add dev error print

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
 
 <?php
 
+use app\controller\HomeController;
+use app\controller\UserController;
+use app\Environment;
 use app\routing\Router;
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
@@ -11,8 +14,8 @@ require __DIR__ . '/vendor/autoload.php';
 $loop = Factory::create();
 
 $router = (new Router())
-    ->registerController(new \app\controller\HomeController())
-    ->registerController(new \app\controller\UserController())
+    ->registerController(new HomeController())
+    ->registerController(new UserController())
     ->resolveRoutes();
 
 $server = new Server($loop, function (ServerRequestInterface $request) use ($router) {
@@ -21,6 +24,10 @@ $server = new Server($loop, function (ServerRequestInterface $request) use ($rou
 
 $socket = new \React\Socket\Server('0.0.0.0:3000', $loop);
 $server->listen($socket);
+
+if(Environment::isProd()){
+    $server->on('error', 'printf');
+}
 
 echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
 
