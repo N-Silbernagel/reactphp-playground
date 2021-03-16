ac0cf3990122f50f0ddfd4a48a11bae4eb746a22
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2021-03-16 19:51:02 +0100

    strict types

--- src/controller/TestController.php
+++ src/controller/TestController.php
@@ -6,7 +6,7 @@ namespace app\controller;
 
 use app\routing\JsonResponse;
 use app\routing\Router;
-use Psr\Http\Message\ServerRequestInterface;
+use Carbon\Carbon;
 use React\Http\Message\Response;
 
 class TestController implements Controller
@@ -14,16 +14,18 @@ class TestController implements Controller
     #[Route('sync', [Router::GET])]
     public static function sync(): Response
     {
+        $initTime = Carbon::now();
         return JsonResponse::generate([
-            "data" => "here",
+            "took" => $initTime->diffInMicroseconds()
         ]);
     }
 
     #[Route('async', [Router::GET])]
     public static function async(): Response
     {
+        $initTime = Carbon::now();
         return JsonResponse::generate([
-            "data" => "here",
+            "took" => $initTime->diffInMicroseconds()
         ]);
     }
 }
